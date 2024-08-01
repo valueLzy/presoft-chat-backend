86b818df9db43b5418801e50fdab922e5fd72b2d
lzy <2339480417@qq.com>
2024-08-01 15:08:21 +0800

    fix

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
+import os
+import uuid
+
+from minio import Minio
 from passlib.context import CryptContext
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+minio_client = Minio(
+    "192.168.1.21:19000",
+    access_key="minioadmin",
+    secret_key="minioadmin",
+    secure=False,
+)
 
 
 def get_hashed_password(password: str) -> str:
@@ -11,5 +22,21 @@ def verify_password(password: str, hashed_pass: str) -> bool:
     return password_context.verify(password, hashed_pass)
 
 
+def download_file(bucket_name: str, file_name: str) -> str:
+    unique_id = str(uuid.uuid4())
+    folder_path = f'./data/{unique_id}'
+    os.makedirs(folder_path, exist_ok=True)
+    file_path = os.path.join(folder_path, file_name)
+    try:
+        minio_client.fget_object(
+            bucket_name=bucket_name,
+            object_name=file_name,
+            file_path=file_path,
+        )
+        return str(file_path)
+    except Exception as e:
+        return ""
+
+
 if __name__ == '__main__':
-    print(get_hashed_password('123456'))
+    print(download_file("vue-file","text2.xlsx"))
